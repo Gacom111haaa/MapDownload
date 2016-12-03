f40100913f3e460c13eb171e3c90b6796223b929
Gacom111haaa <lampt.dce@gmail.com>
2016-12-03 21:33:04 +0700

    Sửa lại cho giống OpenStreetMap
    Down về dưới dạng path/zoom/x/y.jpg

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zoom = 15;
+            int width = 400;
+            int height = 300;
+            int tdim = 256;
+            double lat = 21.036264;
+            double lng = 105.774866;
+
+            double zn = (double)(1 << zoom);
+            double tx = (lng + 180.0) / 360.0;
+            double ty = (1.0 - Math.Log(Math.Tan(lat * Math.PI / 180.0) +
+                                  1.0 / Math.Cos(lat * Math.PI / 180.0)) / Math.PI) / 2.0;
+            tx = tx * zn;
+            ty = ty * zn;
+
+            int xp = (int) (width / 2 - (tx - Math.Floor(tx)) * tdim);
+            int yp = (int) (height / 2 - (ty - Math.Floor(ty)) * tdim);
+
+            // first tile vertical and horizontal
+            // manh ban do dau tien theo chieu doc va ngang
+            int xa = (xp + tdim - 1) / tdim;
+            int ya = (yp + tdim - 1) / tdim;
+            int xs = (int)(tx) - xa;
+            int ys = (int)(ty) - ya;
+
+            int XX = xs;// xp - xa * tdim;
+            int YY = ys;// yp - ya * tdim;
+
+            //XX = 26020;
+            //YY = 14424;
+
+            int xstart = XX-10;
+            int ystart = YY-10;
+            int xstop = XX+10;
+            int ystop = YY+10;
+
+            string url2 = "http://tile.openstreetmap.org/";
+            string url = "https://b.tile.thunderforest.com/cycle/";
+            string path2 = @"D:\DOWN\MapData\OpenStreetMap\";
+            string path = @"D:\DOWN\MapData\ThunderForest\";
+
+
+            try
+            {
+                for (int i = xstart; i < xstop; i++)
+                    for (int j = ystart; j < ystop; j++)
+                    {
+                        string urldown = url + zoom + "/" + i + "/" + j + ".png";
+                        string pathdown = path + zoom + @"\" +i;
+                        if (!Directory.Exists(pathdown))
+                        {
+                            Directory.CreateDirectory(pathdown);
+                        }
+
+                        pathdown = pathdown +  @"\" + j + ".png";
+                        using (var client = new WebClient())
+                        {
+                            client.DownloadFile(urldown, pathdown);
+                        }
+                    }
+            }
+            catch(Exception _ex)
+            {
+                Console.WriteLine(_ex.ToString());
+            }
+        }
+    }
+}
--- ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication1")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication1")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6b592cd6-2d3f-4a61-8124-de57a4f1b060")]
+[assembly: Guid("977a2b5d-26e2-44d4-b32b-61c7bedff1c0")]
 
 // Version information for an assembly consists of the following four values:
 //
